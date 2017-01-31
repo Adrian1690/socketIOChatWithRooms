64382e89c376e8edb3bb509b199d388077fb68a0
Adrian Peña <j.adrian.1690@gmail.com>
2017-01-31 10:16:33 -0500

    back end v0.4

--- app.js
+++ app.js
@@ -14,6 +14,8 @@ var util = require('./util/functions');
 //var session = require('express-session');//
 var io = require('socket.io')(http); 
 
+var connections = [];
+
 // all environments
 app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
@@ -25,9 +27,85 @@ app.get('/', routes.index);
 //app.get('users', routes.user.list);
 
 io.on('connect', function(socket){
-	console.log('user connected');
+	connections.push(socket);
+	console.log('user connected => ' + connections.length);
+
+	var userid = socket.id;
+	socket.emit('ready'); // emit ready message himself
+
+	console.log(io.sockets.adapter.rooms);
+	socket.emit('rooms', io.sockets.adapter.rooms);  // io.sockets.adapter.rooms => get all rooms from io space
+
+	socket.on('join', function (room){
+		socket.join(room);
+		socket.broadcast.emit('new room', room); // emit all user connected message join user room
+	})
+
+	//change username
+	socket.on('change name', function(data){
+		var name = data.name;
+		var room = data.room;
+		var time = util.getCurrentTime();
+
+		// set username to socket
+		socket.set('username', name, function(){
+			// emit new name only for user in that room
+			socket.in(room).emit( 
+				{ 
+					name : name,
+					time : time
+				}
+			);
+			socket.broadcast.to(room).emit('user change name', 
+				{
+					userId : userid, 
+					name : name, 
+					time : time 
+				}
+			);
+		}); 
+	});
+
+	socket.on('send message', function(data){
+		var message = data.message;
+		var time = util.getCurrentTime();
+		var room = data.room;
+		var name = '';
+		socket.get('username', function(err, username){
+			name = username ? username : userid;
+
+			socket.in(room).emit('message sent', { message: message, time: time});
+			socket.broadcast.to(room).emit('message sent by user', {message: message, name: name, time: time});
+		});
+	});
+
 	socket.on('disconnect', function(){
-		console.log('User desconnected');
+
+		// this returns a list of all rooms this user is in
+		//var rooms = io.sockets.adapter.roomClients[socket.id];
+		console.log('=========== socket rooms =========');
+		console.log(socket.rooms);
+		
+		var rooms = socket.rooms;
+		//socket.rooms.forEach(function(room){
+		for (var room in rooms) {	
+	      	// For each room the user is in, excluding his own room
+		    socket.leave(room);
+
+		    socket.get('username', function(err, username){
+		    	if(username){
+		    		socket.broadcast.to(room).emit('user leave room', {name: username});
+		    	}
+		    });  
+		}    
+	    //});
+		
+		connections.splice(connections.indexOf(socket), 1);
+		console.log('User desconnected, now user connected => ' + connections.length);
+
+		setTimeout(function(){
+			socket.broadcast.emit('rooms', io.sockets.adapter.rooms);
+		}, 1000)
 	})
 })
 
